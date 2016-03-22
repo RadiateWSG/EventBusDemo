621d0a21bc87bf880bc62e0a97607ffdbfc23a51
RadiateWSG <wsg1229@qq.com>
2016-03-22 15:02:46 +0800

    负责订阅者的响应函数（onEvent类函数）的信息存储和查找

--- eventbuslib/src/main/java/de/greenrobot/event/SubscriberMethodFinder.java
+++ eventbuslib/src/main/java/de/greenrobot/event/SubscriberMethodFinder.java
@@ -26,6 +26,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * 订阅者响应函数信息存储和查找类
+ */
 class SubscriberMethodFinder {
     private static final String ON_EVENT_METHOD_NAME = "onEvent";
 
@@ -57,6 +60,7 @@ class SubscriberMethodFinder {
         synchronized (methodCache) {
             subscriberMethods = methodCache.get(key);
         }
+        //@mark 如果缓存中存在，则直接返回缓存中的该value
         if (subscriberMethods != null) {
             return subscriberMethods;
         }
@@ -66,6 +70,7 @@ class SubscriberMethodFinder {
         StringBuilder methodKeyBuilder = new StringBuilder();
         while (clazz != null) {
             String name = clazz.getName();
+            //@mark 跳过系统类
             if (name.startsWith("java.") || name.startsWith("javax.") || name.startsWith("android.")) {
                 // Skip system classes, this just degrades performance
                 break;
@@ -73,35 +78,47 @@ class SubscriberMethodFinder {
 
             // Starting with EventBus 2.2 we enforced methods to be public (might change with annotations again)
             Method[] methods = clazz.getDeclaredMethods();
+            //@mark 遍历subscriberClass中的每个方法
             for (Method method : methods) {
                 String methodName = method.getName();
+                //@mark 如果方法名以“onEvent”开头
                 if (methodName.startsWith(ON_EVENT_METHOD_NAME)) {
                     int modifiers = method.getModifiers();
+                    //@mark 该方法是public的，并且不是ABSTRACT、STATIC、BRIDGE、SYNTHETIC(编译器生成的一些函数修饰符)修饰的。
                     if ((modifiers & Modifier.PUBLIC) != 0 && (modifiers & MODIFIERS_IGNORE) == 0) {
                         Class<?>[] parameterTypes = method.getParameterTypes();
+                        //@mark 该方法只有一个参数
                         if (parameterTypes.length == 1) {
                             String modifierString = methodName.substring(ON_EVENT_METHOD_NAME.length());
                             ThreadMode threadMode;
+                            //@mark 根据方法名称来决定ThreadMode。
                             if (modifierString.length() == 0) {
+                                //方法名为“onEvent”
                                 threadMode = ThreadMode.PostThread;
                             } else if (modifierString.equals("MainThread")) {
+                                //方法名为“onEventMainThread”
                                 threadMode = ThreadMode.MainThread;
                             } else if (modifierString.equals("BackgroundThread")) {
+                                //方法名为“onEventBackgroundThread”
                                 threadMode = ThreadMode.BackgroundThread;
                             } else if (modifierString.equals("Async")) {
+                                //方法名为“onEventAsync”
                                 threadMode = ThreadMode.Async;
                             } else {
+                                //@mark 在忽略名单中则继续，否则抛出异常
                                 if (skipMethodVerificationForClasses.containsKey(clazz)) {
                                     continue;
                                 } else {
                                     throw new EventBusException("Illegal onEvent method, check for typos: " + method);
                                 }
                             }
+                            //获取唯一的参数，即事件类型eventType
                             Class<?> eventType = parameterTypes[0];
                             methodKeyBuilder.setLength(0);
                             methodKeyBuilder.append(methodName);
                             methodKeyBuilder.append('>').append(eventType.getName());
                             String methodKey = methodKeyBuilder.toString();
+                            //@mark eventTypesFound为HasSet，元素不允许重复，所以如果子类没有添加才允许父类添加。
                             if (eventTypesFound.add(methodKey)) {
                                 // Only add if not already found in a sub class
                                 subscriberMethods.add(new SubscriberMethod(method, threadMode, eventType));
@@ -113,8 +130,10 @@ class SubscriberMethodFinder {
                     }
                 }
             }
+            //@mark 遍历父类
             clazz = clazz.getSuperclass();
         }
+        //@mark subscriberMethods为空则抛出异常，否则放入缓存。
         if (subscriberMethods.isEmpty()) {
             throw new EventBusException("Subscriber " + subscriberClass + " has no public methods called "
                     + ON_EVENT_METHOD_NAME);
